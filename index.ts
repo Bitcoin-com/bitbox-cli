8c604b41a39c2668a8d61ecfdb083599afc33a16
Gabriel Cardona <gabriel@bitcoin.com>
2019-06-06 12:45:18 +0900

    Clean up deps.

--- index.ts
+++ index.ts
@@ -25,7 +25,7 @@ interface NewOptions extends ConsoleOptions {
   restURL: string
 }
 
-program.version("2.0.2 ", "-v, --version")
+program.version("2.0.3 ", "-v, --version")
 
 program
   .command("new <name>")
